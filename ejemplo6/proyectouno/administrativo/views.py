2cc10b13eeadb6b22be3ec89b9118f310194ac3c
PabloC2A <pabloandrescriolloalvarez@gmail.com>
2025-06-25 09:16:03 -0500

    subida

---
+++ ejemplo6/proyectouno/administrativo/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render
+from django.db import models
+
+# importar las clases de models.py
+from administrativo.models import Matricula, Estudiante
+from administrativo.forms import MatriculaForm, MatriculaEditForm
+
+# vista que permita presesentar las matriculas
+# el nombre de la vista es index.
+
+def index(request):
+    matriculas = Matricula.objects.all()
+    estudiantes = Estudiante.objects.prefetch_related('lasmatriculas__modulo')
+
+    for est in estudiantes:
+        total = sum(m.costo for m in est.obtener_matriculas())
+        est.total_costo = total
+
+    titulo = "Listado de matriculas"
+    contexto = {
+        'matriculas': matriculas,
+        'numero_matriculas': matriculas.count(),
+        'mititulo': titulo,
+        'estudiantes': estudiantes,
+    }
+    return render(request, 'index.html', contexto)
+
+
+def detalle_matricula(request, id):
+    """
+
+    """
+
+    matricula = Matricula.objects.get(pk=id)
+    informacion_template = {'matricula': matricula}
+    return render(request, 'detalle_matricula.html', informacion_template)
+
+
+def crear_matricula(request):
+    """
+    """
+    if request.method=='POST':
+        formulario = MatriculaForm(request.POST)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save() # se guarda en la base de datos
+            return redirect(index)
+    else:
+        formulario = MatriculaForm()
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'crear_matricula.html', diccionario)
+
+def editar_matricula(request, id):
+    """
+    """
+    matricula = Matricula.objects.get(pk=id)
+    print("----------matricula")
+    print(matricula)
+    print("----------matricula")
+    if request.method=='POST':
+        formulario = MatriculaEditForm(request.POST, instance=matricula)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(index)
+    else:
+        formulario = MatriculaEditForm(instance=matricula)
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'crear_matricula.html', diccionario)
+
+def detalle_estudiante(request, id):
+    """
+
+    """
+
+    estudiante = Estudiante.objects.get(pk=id)
+    informacion_template = {'e': estudiante}
+    return render(request, 'detalle_estudiante.html', informacion_template)
